a5c6da78c8fb3b99d18c8ff2fac1afc1d2b2cdac
Altan <Altan@ejemplo.com>
2023-10-02 00:57:16 -0300

    update: Boostrap aplicacion

--- myproject/home/urls.py
+++ myproject/home/urls.py
@@ -1,4 +1,4 @@
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 
 from . import views
@@ -7,13 +7,12 @@ app_name = "home"
 
 urlpatterns = [
         path("", views.index, name = "index"),        
-        path("about/", views.about, name = "about"),
-        
-        
+        path("about/", views.about, name = "about"),     
     
     
     
 ]
 
+urlpatterns += staticfiles_urlpatterns()
 
 
